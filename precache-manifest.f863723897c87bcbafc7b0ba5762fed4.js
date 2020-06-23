ab557859305a1409709b9bb7b714c3917cd92e7f
Aditya Kumar <adipan43@gmail.com>
2020-06-24 04:04:03 +0530

    Updates

---
+++ precache-manifest.f863723897c87bcbafc7b0ba5762fed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c6d9977aee333798b0109b0cf77d5c",
+    "url": "/to_read_books_list/index.html"
+  },
+  {
+    "revision": "3a679b77fe2f649f670a",
+    "url": "/to_read_books_list/static/css/main.0335e579.chunk.css"
+  },
+  {
+    "revision": "9001db60bdc5c5ae8a56",
+    "url": "/to_read_books_list/static/js/2.8d228a02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to_read_books_list/static/js/2.8d228a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a679b77fe2f649f670a",
+    "url": "/to_read_books_list/static/js/main.d9cadbfd.chunk.js"
+  },
+  {
+    "revision": "7a47e4873343b360c64d",
+    "url": "/to_read_books_list/static/js/runtime-main.c3950c0d.js"
+  }
+]);
